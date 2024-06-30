25bef3b1ae27c40d31b6194aff20107353ded825
Huy Le <quanglehuy99.case@gmail.com>
2024-06-30 07:16:38 +0000

    test 2

---
+++ models/example/stg_customers.sql
@@ -0,0 +1,10 @@
+WITH stg_customers AS (
+    SELECT 
+        customer_id,
+        CONCAT(first_name,' ', last_name) AS customer_name,
+        email AS email_address,
+        address AS billing_address
+    FROM raw.customer
+
+)
+SELECT * FROM stg_customers
